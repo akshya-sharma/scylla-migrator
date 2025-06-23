81a33affa069157fd639bc46605ab07b8f059d40
Akshya Sharma <akshyasharma@google.com>
2025-06-23 10:48:00 +0000

    Modified logic to ignore field _dynamo_op_type while writing

--- migrator/src/main/scala/com/scylladb/migrator/writers/DynamoDB.scala
+++ migrator/src/main/scala/com/scylladb/migrator/writers/DynamoDB.scala
@@ -13,9 +13,12 @@ import software.amazon.awssdk.services.dynamodb.model.{ AttributeValue, TableDes
 
 import java.util
 
+import scala.jdk.CollectionConverters._
+
 object DynamoDB {
 
   val log = LogManager.getLogger("com.scylladb.migrator.writers.DynamoDB")
+  private val operationTypeColumnName = "_dynamo_op_type"
 
   def writeRDD(target: TargetSettings.DynamoDB,
                renamesMap: Map[String, String],
@@ -50,10 +53,29 @@ object DynamoDB {
       DynamoDBConstants.THROUGHPUT_WRITE_PERCENT,
       target.throughputWritePercent.map(_.toString))
 
+    // Map over the RDD to potentially modify items
+    val processedRdd = rdd.mapValues { itemWritable =>
+      val item = itemWritable.getItem // This is a java.util.Map[String, AttributeValue]
+
+      // Check if this is a PUT operation
+      //if (item.get(operationTypeColumnName) == putOperationAttributeValue) {
+      // If it's a PUT operation, remove the operationTypeColumnName attribute
+      val filteredScalaMap = item.asScala.view.filterKeys(_ != operationTypeColumnName).toMap
+      val modifiedItem = new java.util.HashMap[String, AttributeValue]()
+      filteredScalaMap.foreach { case (k, v) => modifiedItem.put(k, v) }
+      val newItemWritable = new DynamoDBItemWritable()
+      newItemWritable.setItem(modifiedItem)
+      newItemWritable // Return the modified itemWritable
+    //} else {
+    // If it's not a PUT operation (e.g., it's a DELETE or unknown), leave it as is
+    //  itemWritable
+    //}
+    }
+
     val finalRdd =
-      if (renamesMap.isEmpty) rdd
+      if (renamesMap.isEmpty) processedRdd // Use processedRdd here
       else
-        rdd.mapValues { itemWritable =>
+        processedRdd.mapValues { itemWritable =>
           val item = new util.HashMap[String, AttributeValue]()
           itemWritable.getItem.forEach((key, value) => item.put(renamesMap(key), value))
           itemWritable.setItem(item)
